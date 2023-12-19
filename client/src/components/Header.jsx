e458813c5b0b4eca17850e3651258bc3fac935d3
Mohit Kumar Suman <83830347+mohitkrsuman@users.noreply.github.com>
2023-12-20 01:08:07 +0530

    updated the header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user); 
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -32,11 +34,16 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="text-slate-700 hover:underline cursor-pointer">
-              Sign in
-            </li>
+          <Link to='/profile'>
+              {currentUser ? (
+                 <img className="rounded-full h-8 w-8 object-cover" src={currentUser.avatar} alt="profile" />
+              ) : (
+                  <li className="text-slate-700 hover:underline">Sign in</li>
+              )}
           </Link>
+         
+           
+
         </ul>
       </div>
     </header>
